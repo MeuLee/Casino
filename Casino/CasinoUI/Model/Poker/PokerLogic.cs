bcbba087db9ee3aadc3113ece188b3e2b51dc6f5
Mono98ui <49766423+Mono98ui@users.noreply.github.com>
2019-09-24 09:59:56 -0400

    AddPokerAction
    Added call check raise fold skeleton

--- Casino/CasinoUI/Model/Poker/PokerLogic.cs
+++ Casino/CasinoUI/Model/Poker/PokerLogic.cs
@@ -7,5 +7,21 @@ using System.Threading.Tasks;
 namespace CasinoUI.Model.Poker {
     public class PokerLogic {
          
+        private void Check()
+        {
+
+        }
+        private void Fold()
+        {
+
+        }
+        private void Raise()
+        {
+
+        }
+        private void Call()
+        {
+
+        }
     }
 }
