4b8f4dd31157f4fc437d6d45270f1ee7f4170b43
Antoine Ho <antoineho6@gmail.com>
2019-09-29 22:02:36 -0400

    Set game plan for the poker game flow

--- Casino/CasinoUI/Model/Poker/PokerLogic.cs
+++ Casino/CasinoUI/Model/Poker/PokerLogic.cs
@@ -14,6 +14,24 @@ namespace CasinoUI.Model.Poker {
 
         public PokerLogic(HumanPlayer Human) {
             this.Human = Human;
+            InitListPlayers();
+            SetInitialRoles();
+        }
+
+        private void GameFlow() {
+            // 1. Distribute cards to players
+            // 2. small blind deposits
+            // 3. Big blind deposits
+            // 4. firstPlayer decides to call, fold or raise
+            // 5. other players do the same...
+            // 6. back to small blind: He decides if he wants to call, raise or fold
+            // 7. back to big blind: He decides if he wants to raise or fold if someone raised
+            // 8. Place first 3 cards on table
+            // 9. rotate through players for them to play...
+            // 10. go to second round... (NOTE: check if everybody but one folded or not)
+            // 11. third... (NOTE: check if everybody but one folded or not)
+            // 12. Declare winner and gibe him the pot
+            // 13. proceed to next turn
         }
 
         private void InitListPlayers() {
@@ -23,5 +41,52 @@ namespace CasinoUI.Model.Poker {
                 ListPlayers[i] = new PokerAI();
             }
         }
+
+        private void SetInitialRoles() {
+            SmallBlind = 0;
+            BigBlind = 1;
+            FirstPlayer = 2;
+        }
+
+        private void ProceedNextTurn() {
+            RotateRoles();
+            ClearHands();
+            // restore deck and shuffle
+        }
+
+        private void RotateRoles() {
+            int listLength = ListPlayers.Count;
+
+            // Can refactor into array
+            if (SmallBlind == listLength) {
+                SmallBlind = 0;
+            }
+            else {
+                SmallBlind++;
+            }
+
+            if (BigBlind == listLength) {
+                BigBlind = 0;
+            }
+            else {
+                BigBlind++;
+            }
+
+            if (FirstPlayer == listLength) {
+                FirstPlayer = 0;
+            }
+            else {
+                FirstPlayer++;
+            }
+        }
+
+        private void ClearHands() {
+            foreach (Player player in ListPlayers) {
+                player.Cards.Clear();
+            }
+        }
     }
 }
+
+/// TODO: 1. make initial blinds randomly chosen
+///       2. Restore deck and shuffle
