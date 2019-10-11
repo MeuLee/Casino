599da40f96203a50e0c7d1bb4d22a01d7ffd05f6
Dogetoki <49411824+Dogetoki@users.noreply.github.com>
2019-10-11 09:31:17 -0400

    Class creation + Implemented some of BlackjackLogic

---
+++ Casino/CasinoUI/Models/Blackjack/BlackjackAI.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoUI.Model.Blackjack {
+    public class BlackjackAI : Player {
+
+    }
+}
---
+++ Casino/CasinoUI/Models/Blackjack/BlackjackLogic.cs
@@ -0,0 +1,84 @@
+﻿using CasinoUI.Model.Cards;
+using System.Collections.Generic;
+using System;
+using CasinoUI.Models.Blackjack;
+
+namespace CasinoUI.Model.Blackjack
+{
+    public class BlackjackLogic
+    {
+        private HumanPlayer Human;
+        public List<Player> ListPlayers { get; set; }
+        public GameCardStack CardStack { get; set; }
+
+        public int Pot { get; set; }
+
+        public BlackjackLogic(HumanPlayer Human)
+        {
+            this.Human = Human;
+            InitListPlayers();
+            CardStack = new GameCardStack();
+            Pot = 0;
+        }
+
+        private void GameFlow()
+        {
+            DistributeCards();
+        }
+
+        private void InitListPlayers()
+        {
+            ListPlayers[0] = Human;
+            ListPlayers[1] = new BlackjackAI();
+        }
+
+        private void DistributeCards()
+        {
+            foreach (Player player in ListPlayers)
+            {
+                CardStack.PlayerDrawCard(player);
+                CardStack.PlayerDrawCard(player);
+            }
+        }
+
+        private void ProceedNextTurn()
+        {
+            ClearHands();
+            Pot = 0;
+        }
+
+        private void ClearHands()
+        {
+            foreach (Player player in ListPlayers)
+            {
+                player.Cards.Clear();
+            }
+        }
+
+        private void CurrentPlayerPlay(BlackjackActionCode Action, Player CurrentPlayer)
+        {
+            // TODO: add logic
+            switch (Action)
+            {
+                case BlackjackActionCode.HIT:
+                    CardStack.PlayerDrawCard(CurrentPlayer);                    
+                    break;
+                case BlackjackActionCode.STAND:
+                    break;
+                case BlackjackActionCode.SPLIT:
+                    break;
+                case BlackjackActionCode.INSURANCE:
+                    break;
+                case BlackjackActionCode.DOUBLEDOWN:
+                    break;
+            }
+
+            IncCurrentPlayerTurn();
+        }
+
+        private void IncCurrentPlayerTurn()
+        {
+
+        }
+    }
+}
