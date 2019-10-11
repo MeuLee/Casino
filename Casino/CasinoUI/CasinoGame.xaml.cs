8ad23bf60e804edc4c898e52296c9ae333442294
Antoine Ho <34996565+AntoineHo6@users.noreply.github.com>
2019-10-11 14:54:11 -0400

    Ca compile merci beaoucp bonsoir

---
+++ Casino/CasinoUI/CasinoGame.xaml.cs
@@ -0,0 +1,73 @@
+﻿using CasinoUI.Utils;
+using CasinoUI.View.Map;
+using CasinoUI.View.Map.Tiles;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CasinoUI.View
+{
+    /// <summary>
+    /// Logique d'interaction pour CasinoGame.xaml
+    /// </summary>
+    public partial class CasinoGame : Window
+    {
+        public CasinoGame()
+        {
+            InitializeComponent();
+            InitializeImage();
+        }
+
+        private void InitializeImage()
+        {
+            ImgPlayer.Source = Properties.Resources.panda.ToBitmapImage();
+            TableBackground.ImageSource = Properties.Resources.table.ToBitmapImage();
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            int oldPlayerX = MapRenderer.PlayerX,
+                oldPlayerY = MapRenderer.PlayerY;
+            switch (e.Key)
+            {
+                case Key.A:
+                    if (MapRenderer.PlayerX > 0)
+                    {
+                        MapRenderer.PlayerX--;
+                    }
+                    break;
+                case Key.W:
+                    if (MapRenderer.PlayerY > 0)
+                    {
+                        MapRenderer.PlayerY--;
+                    }
+                    break;
+                case Key.S:
+                    if (MapRenderer.Map.GetLength(1) - 1 > MapRenderer.PlayerY)
+                    {
+                        MapRenderer.PlayerY++;
+                    }
+                    break;
+                case Key.D:
+                    if (MapRenderer.Map.GetLength(0) - 1 > MapRenderer.PlayerX)
+                    {
+                        MapRenderer.PlayerX++;
+                    }
+                    break;
+                default:
+                    return;
+            }
+
+            // could be called in set property
+            OnPlayerMoved(oldPlayerX, oldPlayerY);
+        }
+
+        public void OnPlayerMoved(int oldPlayerX, int oldPlayerY)
+        {
+            GameCanvas.InvalidateVisual();
+            MiniMapCanvas.InvalidateVisual();
+            MapRenderer.Map[MapRenderer.PlayerX, MapRenderer.PlayerY]
+                       .OnMovedOver?
+                       .Invoke(this, new OnMovedOverEventArgs(oldPlayerX, oldPlayerY));
+        }
+    }
+}
---
+++ Casino/CasinoUI/MainMenu.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CasinoUI.View;
+using System.Windows;
+
+namespace CasinoUI
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            CasinoGame game = new CasinoGame();
+            game.Show();
+            this.Close();
+        }
+
+        private void button_Options_Click(object sender, RoutedEventArgs e)
+        {
+            OptionMenu options = new OptionMenu();
+            options.Show();
+        }
+    }
+}
