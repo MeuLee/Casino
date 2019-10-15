942d22d606073aabe86b3574e30f045b696fed8a
MeuLee <49310962+MeuLee@users.noreply.github.com>
2019-10-15 10:49:17 -0400

    Started the menu option

--- Casino/CasinoUI/Views/OptionsMenu.xaml.cs
+++ Casino/CasinoUI/Views/OptionsMenu.xaml.cs
@@ -11,37 +11,5 @@ namespace CasinoUI.View
         {
             InitializeComponent();
         }
-
-        private void ClickedOption1(object sender, RoutedEventArgs e)
-        {
-            btnOpt1.Opacity = 1;
-            btnOpt2.Opacity = 0.25;
-            btnOpt3.Opacity = 0.25;
-            btnOpt4.Opacity = 0.25;
-        }
-
-        private void ClickedOption2(object sender, RoutedEventArgs e)
-        {
-            btnOpt1.Opacity = 0.25;
-            btnOpt2.Opacity = 1;
-            btnOpt3.Opacity = 0.25;
-            btnOpt4.Opacity = 0.25;
-        }
-
-        private void ClickedOption3(object sender, RoutedEventArgs e)
-        {
-            btnOpt1.Opacity = 0.25;
-            btnOpt2.Opacity = 0.25;
-            btnOpt3.Opacity = 1;
-            btnOpt4.Opacity = 0.25;
-        }
-
-        private void ClickedOption4(object sender, RoutedEventArgs e)
-        {
-            btnOpt1.Opacity = 0.25;
-            btnOpt2.Opacity = 0.25;
-            btnOpt3.Opacity = 0.25;
-            btnOpt4.Opacity = 1;
-        }
     }
 }
