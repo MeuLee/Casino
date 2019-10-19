8a0b69fee2740dd184682d8ee748d613c7fd849e
MeuLee <49310962+MeuLee@users.noreply.github.com>
2019-10-19 14:50:14 -0400

    Changed MainMenu's buttons style
    Next:
    1. Create template button instead of copying the same thing over and over again
    2.  Disable the mouseover effect. Or change the font to bold or change the chips color

--- Casino/CasinoUI/Views/MainMenu.xaml.cs
+++ Casino/CasinoUI/Views/MainMenu.xaml.cs
@@ -1,5 +1,7 @@
-﻿using CasinoUI.View;
+﻿using CasinoUI.Utils;
+using CasinoUI.View;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace CasinoUI
 {
@@ -8,23 +10,38 @@ namespace CasinoUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly BitmapImage _casinoChip = Properties.Resources.casinoChip.ToBitmapImage();
         public MainWindow()
         {
             InitializeComponent();
-            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            SetImages();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        // S/O answers did not work.
+        private void SetImages()
         {
-            CasinoGame game = new CasinoGame();
-            game.Show();
-            this.Close();
+            ImgChip1.Source = _casinoChip;
+            ImgChip2.Source = _casinoChip;
+            ImgChip3.Source = _casinoChip;
+            ImgChip4.Source = _casinoChip;
+            ImgChip5.Source = _casinoChip;
+            ImgChip6.Source = _casinoChip;
         }
 
-        private void button_Options_Click(object sender, RoutedEventArgs e)
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
         {
-            OptionMenu options = new OptionMenu();
-            options.Show();
+            new CasinoGame().Show();
+            Close();
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            new OptionMenu().Show();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
