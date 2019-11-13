f2d984557f9ca1aaf967534d380e82d4609d82cb
BonaparteNapoleon <49766183+BonaparteNapoleon@users.noreply.github.com>
2019-11-13 15:04:38 -0500

    Debut test handEvaluator

---
+++ Casino/TestCasino/TesthandEvaluator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CasinoUI.Models.Cards;
+
+
+namespace TestCasino
+{
+    [TestClass]
+    public class TesthandEvaluator
+    {
+        private Card[] jeuDeCarte;
+
+        [TestMethod]
+        public void isRoyalFlush()
+        {
+            jeuDeCarte[0] = new Card(Card.CardRank.Ace, Card.CardSuit.Hearts);
+            
+            Assert.Equals((int)jeuDeCarte[0].Value, (int)Card.CardRank.King);
+        }
+    }
+}
