6393ce2a50a7afabe95d54dd1df53cf60f9a0243
BonaparteNapoleon <49766183+BonaparteNapoleon@users.noreply.github.com>
2019-11-29 12:03:22 -0500

    Revert "Revert "Merge issues commit""
    This reverts commit 7c3244c7352bf8c106866f281bfd51f072f64224.

--- Casino/CasinoUI/Models/Poker/Evaluator/HandStrength.cs
+++ Casino/CasinoUI/Models/Poker/Evaluator/HandStrength.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CasinoUI.Models.Cards;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,29 @@ namespace CasinoUI.Models.Poker.Evaluator
 {
     public struct HandStrength
     {
-        public int Total { get; set; }
-        public int HighCard { get; set; }
+        public HandStrength(int tailleMain)
+        {
+            HandPlayer = new Card[tailleMain];
+            HighCard = null;
+            Total = 0;
+        }
+
+        public Card[] HandPlayer { get; set; }
+        private int Total { get; set; }
+        public Card HighCard { get; set; }
+
+        public int CalculerTotal()
+        {
+            return TrouverTotal();
+        }
+
+        private int TrouverTotal()
+        {
+            foreach (Card cardMain in HandPlayer)
+            {
+                Total += (int)cardMain.Value;
+            }
+            return Total;
+        }
     }
 }
