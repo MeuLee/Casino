5b1abfd09ef5acd3f631f64b1483814929ae5060
BonaparteNapoleon <49766183+BonaparteNapoleon@users.noreply.github.com>
2019-12-03 10:08:47 -0500

    Changement structure test

---
+++ Casino/TestCasino/Controllers/TestBlackjackController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCasino
+{
+    [TestClass]
+    public class TestBlackjackController
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
