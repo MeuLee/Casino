9561f1d5c259da319caa7ab50571679e5ff2b14f
Antoine Ho <34996565+AntoineHo6@users.noreply.github.com>
2019-12-03 10:57:05 -0500

    Work on poker Controller

--- Casino/CasinoUI/Controllers/PokerController.cs
+++ Casino/CasinoUI/Controllers/PokerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using CasinoUI.Models.Poker.PokerBrains;
+using CasinoUI.Models.Profiles;
 using CasinoUI.Models.Settings;
 using CasinoUI.View;
 
@@ -11,24 +12,34 @@ namespace CasinoUI.Models.Poker
         int playerIdx; 
 
         public PokerController() {
+            ApplicationSettings.HumanPlayer.CurrentProfile = new PokerProfile();
             pokerModel = new PokerLogic(ApplicationSettings.HumanPlayer);
             pokerView = new GamePoker();
             pokerView.Show();
 
             int numPlayers = pokerModel.ListPlayers.Count;
 
+            addEvent();
+
+            // if first turn is not humanPlayer
+            if (pokerModel.currentPlayerTurnIdx != 0) {
+                AIsBeforeHumanPlayTurn();
+            }
+
             // game loop
             //while (true) {
 
             //    // sub game loop
             //    while (pokerModel.arePlayersDone()) {
-            //        // AI shit
+            //         // AI shit
             //        //PlayerPlaysTurn();
             //        pokerModel.incCurrentPlayerTurn();
             //    }
 
             //    pokerModel.ProceedNextGame();
             //}
+
+
         }
 
         private void addEvent() {
@@ -41,17 +52,46 @@ namespace CasinoUI.Models.Poker
             pokerModel.PlayerPlaysTurn(PokerActionCode.CALL, -1);
             pokerModel.incCurrentPlayerTurn();
 
-
+            if (!pokerModel.arePlayersDone())
+            {
+                AIsAfterHumanPlayTurn();
+            }
+            else {
+                // split pot
+            }
         }
 
         private void BtnRaise_Click(object sender, RoutedEventArgs e) {
             pokerModel.PlayerPlaysTurn(PokerActionCode.RAISE, 22);
             pokerModel.incCurrentPlayerTurn();
+
+            if (!pokerModel.arePlayersDone()) {
+                AIsAfterHumanPlayTurn();
+            }
         }
 
         private void BtnFold_Click(object sender, RoutedEventArgs e) {
             pokerModel.PlayerPlaysTurn(PokerActionCode.FOLD, -1);
             pokerModel.incCurrentPlayerTurn();
+
+            if (!pokerModel.arePlayersDone()) {
+                AIsAfterHumanPlayTurn();
+            }
+        }
+
+        private void AIsBeforeHumanPlayTurn() {
+            while (pokerModel.currentPlayerTurnIdx != 0)
+            {
+                pokerModel.PlayerPlaysTurn(PokerActionCode.CALL, -1);   // temp
+                pokerModel.incCurrentPlayerTurn();
+            }
+        }
+
+        private void AIsAfterHumanPlayTurn() {
+            while (pokerModel.currentPlayerTurnIdx != pokerModel.PlayerRoles[2]) {
+                pokerModel.PlayerPlaysTurn(PokerActionCode.CALL, -1);   // temp
+                pokerModel.incCurrentPlayerTurn();
+            }
         }
     }
 }
